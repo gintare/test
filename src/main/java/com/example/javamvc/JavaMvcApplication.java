356eeeff09260c6711badcabde6450dded8f59e4
LAPTOP-IHFLTACV\daiva <mar.kazlauskas@gmail.com>
2023-03-13 09:08:17 +0200

    init

---
+++ src/main/java/com/example/javamvc/JavaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javamvc/JavaMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
